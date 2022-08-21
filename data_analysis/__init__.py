258d6c16a45af764ba2e5d54a81e2e2d2d64b5c3
Adam Wiveion <wux024@nenu.edu.cn>
2022-08-21 17:00:47 +0800

    Create __init__.py

---
+++ data_analysis/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author: Adam Wiveion
+@contact: wux024@nenu.edu.cn
+@time: 2022/8/21 14:11
+"""
+from .fourier import fourier_analysis
+from .angles import compute_pose_angles
+from .deriv import cal_deriv
